17618157d0fc4adf8e6cc91917da297823920c68
Administrator <Pathakshivnandan0312@gmail.com>
2026-07-11 17:05:15 -0400

    update array Default comment Program

--- src/ArrayDefaultValues.java
+++ src/ArrayDefaultValues.java
@@ -7,8 +7,9 @@ public class ArrayDefaultValues {
         int[] arr = new int[4];
         arr[0] = 50; // Memory:Index : 0   1   2   3
         arr[3] = 100; //      Value :50   0   0 100
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) { // Size = 4, Last Index = Length Size-1
             System.out.println(arr[i]);
         }
     }
 }
+
